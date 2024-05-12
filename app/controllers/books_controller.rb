10ed4a4cdbc380dab7eedd000823368992c503ec
HannaKitora <h.wimper@gmail.com>
2024-05-12 13:43:50 +0000

    20240512cantcreatbook

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,56 @@
 class BooksController < ApplicationController
   def top
   end
+  
+  def new
+    @book = Book.new
+  end
+ 
+  def create
+    @book = Book.new(book_params)
+    
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      redirect_to book_path(@book.id)
+    else
+      flash.now[:alart] = "2 errors prohibited this book from being saved:Title can't be blank Body can't be blank"
+      @books = Book.all
+      render :index
+    end
+  end
+  
+
+  def index
+    @books = Book.all
+    @book = Book.new
+    
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+  
+  def update
+    @book = Book.new #追加
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)
+  end
+  
+  def delete
+  end
+  
+  def start
+    redirect_to books_path
+  end
+  
+  private
+  
+  def book_params
+    params.require(:book).permit(:title, :body)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
+  # get 'lists/new'
+  # get 'lists/index'
+  # get 'lists/show'
+  # get 'lists/edit'
   get '/top' => 'homes#top'
+  resources :books
   # For details on the DSL available wiget 'top' => 'homes#top'
 end
